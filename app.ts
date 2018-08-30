a9b8d57e666c334f1e1087a1e1ae2c2deacecba4
Tom Forster <tom.forster@mpec.co.uk>
2018-08-30 13:38:43 +0100

    hope to fix path

--- app.ts
+++ app.ts
@@ -6,6 +6,7 @@ import {connectionPromise} from "./db";
 import {itemService, Routes} from "./routes/routes";
 import corser = require("corser");//for cors
 import "pug";
+import * as path from "path";
 
 export class Page<T>
 {
@@ -37,7 +38,7 @@ export const appPromise = connectionPromise.then(async connection =>
     
     app.use(corser.create());
     
-    app.use(express.static('static'));
+    app.use(express.static(path.join("process.env.PWD", 'static')));
     app.set('view engine', 'pug');
     
     // register all application routes
