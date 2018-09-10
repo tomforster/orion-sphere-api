4af9fff6d8af07b0e0ac797bd15102595f8e3145
Tom Forster <tom.forster@mpec.co.uk>
2018-09-10 17:22:33 +0100

    more work on select box

--- webapp/src/views/TestView.ts
+++ webapp/src/views/TestView.ts
@@ -7,14 +7,17 @@ export class TestView implements ClassComponent
     value:string;
     timeout:any;
     results:IItemModel[] = [];
+    loading:boolean = false;
+    selectedItem:IItemModel;
     
     oninit(vnode:Vnode):any
     {
-    
+        this.onValueChange("");
     }
     
     onValueChange(value:string):any
     {
+        this.loading = true;
         this.value = value;
         if(this.timeout) clearTimeout(this.timeout);
         this.timeout = setTimeout(() => {
@@ -22,13 +25,15 @@ export class TestView implements ClassComponent
             {
                 this.results = [];
                 m.redraw();
+                this.loading = false;
                 return;
             }
-            m.request({method:"get", url:"/item-models", data:{s:{s:this.value}, page:0, size:10}})
+            m.request({method:"get", url:"/item-models", data:{s:{s:this.value}, page:0, size:5}})
                 .then((r:any) => {
                     this.results = r.content;
                     m.redraw();
-                });
+                    this.loading = false;
+                }).catch(e => this.loading = false)
         }, 500);
     }
     
@@ -37,12 +42,52 @@ export class TestView implements ClassComponent
         if(this.timeout) clearTimeout(this.timeout);
     }
     
+    focused:boolean = false;
+    
+    onFocus()
+    {
+        this.focused = true;
+    }
+    
+    onFocusOut()
+    {
+        this.focused = false;
+    }
+    
+    onOptionPress(item:IItemModel)
+    {
+        this.selectedItem = item;
+        this.focused = false;
+    }
+    
     view(vnode:Vnode)
     {
-        return m(".container", m(".box", [
-            m(".field", m(".control", m("input.input", {value: this.value, oninput: m.withAttr("value", this.onValueChange.bind(this))}))),
-            ...this.results.map(r => m("", r.name))
-            ])
+        return m(".container", [
+            m(".field", {style: "margin-bottom: 0.5em"},
+                m(".control", [
+                    m("label.label.is-small", "Search Models"),
+                    !this.selectedItem ?
+                        m("input.input", {
+                            value: this.value,
+                            onfocusout: this.onFocusOut.bind(this),
+                            onfocus: this.onFocus.bind(this),
+                            oninput: m.withAttr("value", this.onValueChange.bind(this))
+                        }) :
+                        m("input.input", {
+                            value: this.selectedItem.name,
+                            readonly: true
+                        })
+                ])
+            ),
+            m("", {class: this.focused ? "" : "is-hidden"},
+                this.value ?
+                    this.loading ?
+                        m("", "Loading...") :
+                        this.results.length ?
+                            this.results.map(r => m(".select-option", {onmousedown:this.onOptionPress.bind(this, r)}, r.name)) :
+                            m("", "No results found.") :
+                    m("", "Type to search..."))
+            ]
         );
     }
 }
