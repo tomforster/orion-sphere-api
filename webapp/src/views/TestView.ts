acfb5152df32ad399fef497a715e55b61cb160d8
Tom <tomforster@gmail.com>
2018-09-12 22:16:42 +0100

    selectable items in test view

--- webapp/src/views/TestView.ts
+++ webapp/src/views/TestView.ts
@@ -11,7 +11,8 @@ export class TestView implements ClassComponent
     timeout:any;
     page:Page<IItemModel>;
     loading:boolean = false;
-    selectedItem:IItemModel;
+    selectedItemId:number;
+    currentPage:number = 0;
     
     oninit(vnode:Vnode):any
     {
@@ -23,7 +24,7 @@ export class TestView implements ClassComponent
         this.loading = true;
         if(this.timeout) clearTimeout(this.timeout);
         this.timeout = setTimeout(() => {
-            m.request({method:"get", url:"/item-models", data:{s:this.filterOptions, page:0, size:15}})
+            m.request({method:"get", url:"/item-models", data:{s:this.filterOptions, page:this.currentPage, size:15}})
                 .then((r:any) => {
                     this.page = r;
                     m.redraw();
@@ -37,22 +38,9 @@ export class TestView implements ClassComponent
         if(this.timeout) clearTimeout(this.timeout);
     }
     
-    focused:boolean = false;
-    
-    onFocus()
-    {
-        this.focused = true;
-    }
-    
-    onFocusOut()
-    {
-        this.focused = false;
-    }
-    
     onOptionPress(item:IItemModel)
     {
-        this.selectedItem = item;
-        this.focused = false;
+        this.selectedItemId = item.id;
     }
     
     itemType:ItemType;
@@ -69,37 +57,73 @@ export class TestView implements ClassComponent
         this.fetch();
     }
     
+    onNextPress()
+    {
+        if(!this.page.last)
+        {
+            this.currentPage++;
+            this.fetch();
+        }
+    }
+    
+    onFirstPress()
+    {
+        if(!this.page.first)
+        {
+            this.currentPage = 0;
+            this.fetch();
+        }
+    }
+    
+    onLastPress()
+    {
+        if(!this.page.last)
+        {
+            this.currentPage = this.page.totalPages-1;
+            this.fetch();
+        }
+    }
+    
+    onPreviousPress()
+    {
+        if(!this.page.first)
+        {
+            this.currentPage--;
+            this.fetch();
+        }
+    }
+    
     getPaging(page:Page<IItemModel>):Vnode
     {
         return m(".columns", [
-            m(".column.is-narrow", m(`button.button`, {disabled: page.first, onclick: (e:any) => { page.first && e.preventDefault() }}, "First")),
-            m(".column.is-narrow", m(`button.button`, {disabled: page.first, onclick: (e:any) => { page.first && e.preventDefault() }}, "Previous")),
+            m(".column.is-narrow", m(`button.button`, {disabled: page.first, onclick: this.onFirstPress.bind(this)}, "First")),
+            m(".column.is-narrow", m(`button.button`, {disabled: page.first, onclick: this.onPreviousPress.bind(this)}, "Previous")),
             m(".column.is-vcentered.is-flex", {style: "justify-content: center"}, `${page.number+1}/${page.totalPages}`),
-            m(".column.is-narrow", m(`button.button]`, {disabled: page.last, onclick: (e:any) => { page.last && e.preventDefault() }}, "Next")),
-            m(".column.is-narrow", m(`button.button`, {disabled: page.last, onclick: (e:any) => { page.last && e.preventDefault() }}, "Last"))
+            m(".column.is-narrow", m(`button.button`, {disabled: page.last, onclick: this.onNextPress.bind(this)}, "Next")),
+            m(".column.is-narrow", m(`button.button`, {disabled: page.last, onclick: this.onLastPress.bind(this)}, "Last"))
         ]);
     }
     
     view(vnode:Vnode)
     {
-        return m(".container", [
-            m(".field", [
-                m('label.label.is-small', "Search"),
-                m('.control.is-expanded', m("input.input[type='text']", {value: this.filterOptions.s, placeholder: 'Filter on name...', oninput: m.withAttr("value", this.onSearchChange.bind(this))})),
-            ]),
-            m(".field.is-horizontal", m(".field-body", [
-                m(".field", [
+        return m(".container.search", [
+            m(".columns", {style:"margin-bottom:0px"}, [
+                m(".column", m(".field", [
+                    m('label.label.is-small', "Search"),
+                    m('.control', m("input.input[type='text']", {value: this.filterOptions.s, placeholder: 'Filter on name...', oninput: m.withAttr("value", this.onSearchChange.bind(this))})),
+                ])),
+                m(".column.is-narrow", m(".field", [
                     m("label.label.is-small", "Item Type"),
-                    m('.control',
+                    m('.control.is-expanded',
                         m(".select",
                             m(`select`, {onchange: m.withAttr("value", this.onItemTypeChange.bind(this))},
                                 [m('option'), ...Object.keys(ItemType).map(typeKey => m('option', {value: typeKey, selected: this.itemType === typeKey}, ItemType[<any>typeKey]))]
                             )
                         )
                     )]
-                )
-            ])),
-            ...(this.page ? this.page.content.map(r => m("", r.name)) : [m("")]),
+                ))
+            ]),
+            m(".box", this.page ? this.page.content.map(r => m(".select-option", {class: this.selectedItemId === r.id ? "selected" : "", onclick: this.onOptionPress.bind(this, r)}, r.name)) : m("")),
             this.page ? this.getPaging(this.page) : m("")
         ]);
     }
