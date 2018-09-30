d40ce2b8594559e0c4864f7268fb801e8a6e3663
Tom <tomforster@gmail.com>
2018-09-30 11:02:42 +0100

    ability filtering

--- service/AbilityService.ts
+++ service/AbilityService.ts
@@ -1,7 +1,32 @@
 import {Ability} from "../entity/Ability";
 import {Service} from "./Service";
+import {FilterOptions} from "./filters/FilterOptions";
+import {Page} from "../Page";
+import {Brackets} from "typeorm";
 
 export class AbilityService extends Service<Ability>
 {
     entityClass:any = Ability;
+    
+    async findAll(page:number, size:number, filterOptions:FilterOptions):Promise<Page<Ability>>
+    {
+        let query = this.getRepository()
+            .createQueryBuilder("ability")
+            .where("1=1");
+        
+        if(filterOptions.s)
+        {
+            const s = "%" + filterOptions.s + "%";
+            query = query.andWhere(new Brackets(qb => {
+                qb.where("LOWER(ability.description) like LOWER(:s)", {s})
+            }));
+        }
+        
+        query.skip(page*size);
+        query.take(size);
+        
+        const [result, count] = await query.getManyAndCount();
+        
+        return new Page<Ability>(result.map(i => {(i as any).type = this.entityClass.name; return i}), page, size, count);
+    }
 }
